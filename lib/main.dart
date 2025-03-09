7c470eb678214053cb28cc4ac43f6c07d7278cfc
Robbie <rsmi315@lsu.edu>
2025-03-09 17:53:08 -0500

    icon and title change

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App',
+      title: 'PFT Scavenger',
       // Define the initial route
       initialRoute: '/',
       // Define routes
